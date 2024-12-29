4aaa9df0b6bee7e775919808bfe561b6259022b9
Fed12 <fedor.cherinko@gmail.com>
2024-12-29 20:53:31 +0200

    added ErrorSlice

--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-//import booksReducer from "./Books/Reducer";
 import filterReducer from "./slices/FilterSlice";
 import booksReducer from "./slices/BooksSlice";
+import errorReducer from "./slices/ErrorSlice";
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
 });
 
